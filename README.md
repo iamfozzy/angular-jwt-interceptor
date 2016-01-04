a71e7aa39e988b695c889feb9990a0f723bdba77
Stuart <stuart@fozzy.media>
2016-01-04 23:59:04 +0000

    Added auth inteceptor

---
+++ README.md
@@ -0,0 +1,2 @@
+# Angular JWT Auth Interceptor
+Example of a request interceptor for angular allowing you to attach the Bearer header for JWT authentication.
---
+++ auth-interceptor.js
@@ -0,0 +1,20 @@
+
+export default function ($rootScope, $q, $window) {
+  'ngInject'
+
+  return {
+    request: function (config) {
+      config.headers = config.headers || {};
+      if ($window.sessionStorage.token) {
+        config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+      }
+      return config;
+    },
+    response: function (response) {
+      if (response.status === 401) {
+        // handle the case where the user is not authenticated
+      }
+      return response || $q.when(response);
+    }
+  }
+}
